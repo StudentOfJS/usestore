53d62c5dc18e9f02fc0768bf72a355892f7d5638
Rod Lewis <rod.lewis@watercorporation.com.au>
2022-08-13 20:11:44 +0800

    library setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { splitVendorChunkPlugin } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [
+        react(),
+        splitVendorChunkPlugin(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/lib/index.ts'),
+            name: 'useStore',
+            formats: ['es', 'umd'],
+            fileName: (format) => `useStore.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react'],
+            output: {
+                globals: {
+                    react: 'React',
+                },
+            },
+        },
+    },
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,15 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// eslint-disable-next-line import/no-default-export -- by design
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   test: {
+    globals: true,
     environment: 'happy-dom',
     include: ['**/*.test.ts'],
   },
-});
+})
